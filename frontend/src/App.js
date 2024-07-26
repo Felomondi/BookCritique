c308c40a0c987078744f06e72b45393d0c546564
felomondi <fomondi@vassar.edu>
2024-07-26 19:16:03 -0400

    commit react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ const App = () => (
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Register</Link></li>
-        {/* <li><Link to="/homepage">Homepage</Link></li> */}
       </ul>
     </nav>
     <Routes>
